ea0ecd15c00856b46fa5c5d41c48a529a82eff6e
S-Aoi <zjc0816@hotmail.com>
2016-10-14 01:28:47 +0800

    修改打包脚本，打包运行时

--- bat/PackageApp.bat
+++ bat/PackageApp.bat
@@ -7,8 +7,8 @@ set PAUSE_ERRORS=1
 call bat\SetupSDK.bat
 call bat\SetupApp.bat
 
-set AIR_TARGET=
-::set AIR_TARGET=-captive-runtime
+::set AIR_TARGET=
+set AIR_TARGET=-captive-runtime
 set OPTIONS=-tsa none
 call bat\Packager.bat
 
--- bat/Packager.bat
+++ bat/Packager.bat
@@ -11,8 +11,8 @@ set OUTPUT=%AIR_PATH%\%AIR_NAME%%AIR_TARGET%.air
 
 :: Package
 echo.
-echo Packaging %AIR_NAME%%AIR_TARGET%.air using certificate %CERT_FILE%...
-call adt -package %OPTIONS% %SIGNING_OPTIONS% %OUTPUT% %APP_XML% %FILE_OR_DIR%
+echo Packaging %AIR_NAME%%AIR_TARGET% using certificate %CERT_FILE%...
+call adt -package %OPTIONS% %SIGNING_OPTIONS% -target bundle %OUTPUT% %APP_XML% %FILE_OR_DIR%
 if errorlevel 1 goto failed
 goto end
 
